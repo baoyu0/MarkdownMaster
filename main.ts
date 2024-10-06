4dc8eb0ea1dbb395f253b1a0697f915ec06162c2
baoyu0 <zhaidandan00@gmail.com>
2024-10-06 15:28:56 +0800

    modified:   main.ts 	modified:   manifest.json 	modified:   package.json 	modified:   versions.json

--- main.ts
+++ main.ts
@@ -57,7 +57,7 @@ const DEFAULT_SETTINGS: MarkdownMasterSettings = {
 
 
 // 更新插件版本号
-const PLUGIN_VERSION = "1.2.9";
+const PLUGIN_VERSION = "1.4.1";
 
 export default class MarkdownMasterPlugin extends Plugin {
     settings!: MarkdownMasterSettings;
